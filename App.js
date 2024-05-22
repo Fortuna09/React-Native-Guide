c32857c5c3ffe582d8fae12d6846eb92b8a85fcb
Fortuna09 <rafael.fortuna@sou.unifal-mg.edu.br>
2024-05-22 16:54:34 -0300

    feat: in

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Rafael Fortuna</Text>
+      <Text>Rafael</Text>
       <StatusBar style="auto" />
     </View>
   );
